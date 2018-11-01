9eb6684a0d25ba71ffcf8c17556cd561d6b2928b
Nayron Barreto <nayronbarreto@gmail.com>
2018-11-01 17:41:46 +0000

    Initial Style

---
+++ src/styles/colors.js
@@ -0,0 +1,20 @@
+export default {
+  white: '#FFF',
+  lighter: '#EEE',
+  light: '#DDD',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+
+  primary: '#68217a',
+  secundary: '#444A54',
+  success: '#9DCA83',
+  danger: '#E37A7A',
+  green: '#00b050',
+
+transparent: 'transparent',
+darkTransparente: 'rgba(0, 0, 0, 0.6)',
+whiteTransparente: 'rgba(255, 255, 255, 0.3)',
+
+}
